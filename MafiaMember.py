f28d9d9606b3a544e2c1374ddb046873730196e9
KarinSand <karen.h.sand@gmail.com>
2025-10-10 14:44:28 +0200

    Add MafiaMember abstract class

---
+++ MafiaMember.py
@@ -0,0 +1,34 @@
+class MafiaMember: #abstract class
+
+
+    def __init__(self, name: str, age: int, loyalty: int):
+        self._name = name
+        self._age = age
+        self._loyalty = loyalty
+        pass
+
+    def get_name(self) -> str:
+        pass
+
+    def set_name(self, name: str) -> None:
+        pass
+
+    def get_age(self) -> int:
+        pass
+
+    def set_age(self, age: int) -> None:
+        pass
+
+    def get_loyalty(self) -> int:
+        pass
+
+    def set_loyalty(self, loyalty: int) -> None:
+        pass
+
+    @abstractmethod
+    def commit_crime(self, crime: Crime) -> None:
+        pass
+    
+    @abstractmethod
+    def describe(self) -> str:
+        pass
