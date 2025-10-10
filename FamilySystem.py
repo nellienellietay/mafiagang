3beab5811fa68052b8ef433ef145d334103a4836
KarinSand <karen.h.sand@gmail.com>
2025-10-10 14:52:09 +0200

    Add FamilySystem class

---
+++ FamilySystem.py
@@ -0,0 +1,11 @@
+class FamilySystem:
+    
+    def __init__(self):
+        self._inventory = FamilyInventory()
+        
+
+    def run(self) -> None:
+        pass
+
+    def menu(self) -> None:
+        pass
