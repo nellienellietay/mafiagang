abdf5ec2415e830d208a12212eb8fcc42f25d2df
KarinSand <karen.h.sand@gmail.com>
2025-10-10 14:17:34 +0200

    Create Crime.py

---
+++ Crime.py
@@ -0,0 +1,32 @@
+class Crime:
+
+
+  def __init__(self, crime_type: str, target: str, profit: float):
+    self._crime_type = crime_type  
+    self._target = target
+    self._profit = profit
+    self._ assigned_member: ["MafiaMember"]= none
+
+def assign_member(self, member: "MafiaMember") -> None:
+  pass
+
+def get_type(self) -> str:
+  pass
+
+def set_type(self, crime_type:) -> str:
+  pass
+
+def get_target(self) -> str:
+  pass
+
+def set_target(self, target: str) -> None:
+  pass
+
+def get_profit(self) -> float:
+  pass
+
+def set_profit(self, profit: float) -> None:
+  pass
+
+def describe(self) -> str:
+  pass
