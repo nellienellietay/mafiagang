b42fa6c77128ed046145176d5c817f387078f94b
nellienellietay <nellie.elisabeth@gmail.com>
2025-10-16 01:12:22 +0200

    nästan färdig

--- mafia_member.py
+++ mafia_member.py
@@ -1,33 +1,49 @@
+from abc import ABC, abstractmethod
+from crime import Crime
+
 class MafiaMember(ABC): #abstract class
 
 
-    def __init__(self, name: str, age: int, loyalty: int):
-        self._name = name
-        self._age = age
-        self._loyalty = loyalty
-        pass
+    def __init__(self, name: str, age: int, loyalty: int, role: str, reports_to: Optional['MafiaMember'] = None):
+        self.__name = name
+        self.__age = age
+        self.__loyalty = loyalty
+        self.__role = role
+        self.__reports_to = reports_to
 
     def get_name(self) -> str:
-        pass
+        return self.__name
 
-    def set_name(self, name: str) -> None:
-        pass
+    def set_name(self, new_name: str) -> None:
+        self.__name = new_name
 
     def get_age(self) -> int:
-        pass
+        return self.__age
 
-    def set_age(self, age: int) -> None:
-        pass
+    def set_age(self, new_age: int) -> None:
+        self.__age = new_age
 
     def get_loyalty(self) -> int:
-        pass
+        return self.__loyalty
 
-    def set_loyalty(self, loyalty: int) -> None:
-        pass
+    def set_loyalty(self, new_loyalty: int) -> None:
+        self.__loyalty = new_loyalty
+
+    def get_role(self) -> str:
+        return self.__role
+    
+    def set_role(self, new_role: str) -> None:
+        self.__role = new_role
+
+    def get_reports_to(self) -> Optional['MafiaMember']:
+        return self.__reports_to
+    
+    def set_reports_to(self, new_reports_to: 'MafiaMember') -> None:
+        self.__reports_to = new_reports_to
 
     @abstractmethod
     def commit_crime(self, crime: Crime) -> None:
-        pass
+        
     
     @abstractmethod
     def describe(self) -> str:
