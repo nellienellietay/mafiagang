2dfa5acc7327b6f52b909882fb3d0b0a2881f38b
nellienellietay <nellie.elisabeth@gmail.com>
2025-10-20 22:30:18 +0200

    "optional" och kommentarer
    Lagt in någon kommentar och tagit bort optional. Ska anteckna att vi behöver komma ihåg att ta bort d även från UML

--- mafia_member.py
+++ mafia_member.py
@@ -3,8 +3,10 @@ from crime import Crime
 
 class MafiaMember(ABC): #abstract class
 
+        #tagit bort alla "optional" eftersom det inte behövs när man gör type-hinting tydligen
+        #'MafiaMember' | None  betyder att det kan vara antingen ett instance of a class eller None
 
-    def __init__(self, name: str, age: int, loyalty: int, role: str, reports_to: Optional['MafiaMember'] = None):
+    def __init__(self, name: str, age: int, loyalty: int, role: str, reports_to: ['MafiaMember'] | None = None):
         self.__name = name
         self.__age = age
         self.__loyalty = loyalty
@@ -35,10 +37,12 @@ class MafiaMember(ABC): #abstract class
     def set_role(self, new_role: str) -> None:
         self.__role = new_role
 
-    def get_reports_to(self) -> Optional['MafiaMember']:
+    def get_reports_to(self) -> 'MafiaMember' | None: 
+        # returnerar den här medlemmens chef (MafiaMember-objekt) eller None om den inte har någon chef
         return self.__reports_to
     
     def set_reports_to(self, new_reports_to: 'MafiaMember') -> None:
+         # sätter vem den här medlemmen rapporterar till (dvs vem som är chef)
         self.__reports_to = new_reports_to
 
     @abstractmethod
