bea0f11d20765a9208a36434754d4112e92296f9
KarinSand <karen.h.sand@gmail.com>
2025-10-23 12:33:19 +0200

    added some docstringd and  remoed loyalty attribute

--- mafia_member.py
+++ mafia_member.py
@@ -5,42 +5,55 @@ class MafiaMember(ABC): #abstract class
         #tagit bort alla "optional" eftersom det inte behövs när man gör type-hinting tydligen
         #'MafiaMember' | None  betyder att det kan vara antingen ett instance of a class eller None
 
-    def __init__(self, name: str, age: int, loyalty: int, role: str, reports_to = None): # none
+    def __init__(self, name: str, age: int, role: str, reports_to = None): 
+        """Initializes a MafiaMember as instance"""
         self.__name = name
         self.__age = age
-        self.__loyalty = loyalty
         self.__role = role
         self.__reports_to = reports_to
 
     def get_name(self) -> str:
+        """Gets name of mafia member
+        returns: 
+            str name
+        """
         return self.__name
 
     def set_name(self, new_name: str) -> None:
+        """Sets name for mafia member"""
         self.__name = new_name
 
     def get_age(self) -> int:
+        """Gets age of mafia member
+        returns: 
+            int age
+        """
         return self.__age
 
     def set_age(self, new_age: int) -> None:
         self.__age = new_age
 
-    def get_loyalty(self) -> int:
-        return self.__loyalty
-
-    def set_loyalty(self, new_loyalty: int) -> None:
-        self.__loyalty = new_loyalty
-
     def get_role(self) -> str:
+        """Gets role of mafia member
+        returns: 
+            str role
+        """
         return self.__role
     
     def set_role(self, new_role: str) -> None:
+        """Sets role for  mafia member"""
         self.__role = new_role
 
     def get_reports_to(self):  # returns MafiaMember or None
+        """Gets who this member reports to
+        returns:
+            MafiaMember: None
+        """
         return self.__reports_to
         # returnerar den här medlemmens chef (MafiaMember-objekt) eller None om den inte har någon chef
     
     def set_reports_to(self, new_reports_to) -> None:
+        """ Sets who this member reports to"""
          # sätter vem den här medlemmen rapporterar till (dvs vem som är chef)
         self.__reports_to = new_reports_to
 
@@ -48,9 +61,11 @@ class MafiaMember(ABC): #abstract class
     #man skapar aldrig instance of an object av abstract methods
     @abstractmethod
     def commit_crime(self, crime: Crime) -> None:
+        """Abstract method for committing a crime"""
         pass
     
     @abstractmethod
     #ändrat från describe till str, bättre praxis, osäker om den fortfarande behöver vara abstract 
     def __str__(self) -> str:
+        """Abstract method for committing a crime"""
         pass
