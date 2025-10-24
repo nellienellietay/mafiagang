67af07e9073f881230a7c85efa877cc85006249f
KarinSand <karen.h.sand@gmail.com>
2025-10-24 11:42:28 +0200

    Add docstrings to Soldier class methods
    Added docstrings to the Soldier class methods and constructor.

--- soldier.py
+++ soldier.py
@@ -1,19 +1,20 @@
 from mafia_member import MafiaMember
 from crime import Crime, CrimeType
 
-"""This is a subklass to MafiaMember"""
-
 class Soldier(MafiaMember):
+    """Subclass to MafiaMember"""
     
     def __init__(self, name: str, age: int):
-            super().__init__(name, age, "Soldier")
+        """Initializes Soldier with name, age and role"""
+        super().__init__(name, age, "Soldier")
 
-    
     def commit_crime(self, crime: Crime) -> None:
         """commits the crime """
         print(f"{self.get_name()} commits {crime.get_type().value}.")
 
-
     def __str__(self):
+        """Returns string representation of Soldier
+        returns:
+            str representation
+        """
         return f"Soldier {self.get_name()}"
-        
