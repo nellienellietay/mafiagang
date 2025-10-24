a54d3ef71ecaddfbf72199069869679a209711f8
KarinSand <karen.h.sand@gmail.com>
2025-10-24 14:28:46 +0200

    Update godfather.py

--- godfather.py
+++ godfather.py
@@ -1,6 +1,8 @@
 from mafia_member import MafiaMember
 from consigliere import Consigliere
 from capo import Capo
+from crime import Crime, CrimeType
+
 
 class Godfather(MafiaMember):
     """Subclass to MafiaMember"""
@@ -10,11 +12,14 @@ class Godfather(MafiaMember):
         self.__capos = []
         self.__consigliere = None
 
-    def commit_crime(self, crime) -> None:
-        """Godfather does not execute crimes directly.
-        args:
-            crime (Crime): The crime not being committed"""
-        raise PermissionError(f"Godfather {self.get_name()} does not commit crime directly")
+    def commit_crime(self, crime: Crime) -> None:
+        """Godfather may handle racketeering and bribery. Others are refused."""
+        if crime.get_type() == CrimeType.RACKETEERING:
+            print(f"{self.get_name()} can do some racketeering")
+        elif crime.get_type() == CrimeType.BRIBERY:
+            print(f"{self.get_name()} can do some bribery")
+        else:
+            print(f"{self.get_role()} {self.get_name()} does not deal with {crime.get_type().value}")
 
     def __str__(self) -> str:
         """string rep of Godfather
