4a17e97a947df8164dcb8d1a4169ae71a8afe1d4
KarinSand <karen.h.sand@gmail.com>
2025-10-24 11:58:19 +0200

    docstrings
    Updated docstrings for clarity and consistency.

--- capo.py
+++ capo.py
@@ -3,16 +3,20 @@ from crime import Crime, CrimeType
 from soldier import Soldier
 
 class Capo(MafiaMember):
-    """subclass of MafiaMember representing the Capo in the mafia hierarchy."""
+    """Subclass to MafiaMember"""
     def __init__(self, name, age):
         super().__init__(name=name, age=age, role="Capo")
 
     def commit_crime(self, crime: Crime) -> None:
-        """Capo directly handles only crimes appropriate for a Capo."""
+        """Capo directly handles only crimes appropriate for a caporegime."""
         if crime.get_type() == CrimeType.RACKETEERING: 
             print(f"{self.get_name()} can do some Racketeering")
         else:
             print(f"{self.get_role()} {self.get_name()} does not deal with {crime.get_type().value}")
     
     def __str__(self) -> str:
+        """Returns string rep of Capo
+        returns:
+            str representation
+        """
         return f"Capo {self.get_name()}"
